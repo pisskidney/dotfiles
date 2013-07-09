cc1beadf5319284db435f0b0cb629962bb0c305d
Peter Horvath <horvthpeter@gmail.com>
2013-07-09 11:45:04 +0300

    gardening

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,4 +13,4 @@ fi
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 sudo pip install pep8
 sudo pip install pyflakes
-vim +BundleInstall! +BundleClean! +qall
+vim - +BundleInstall! +BundleClean! +qall
